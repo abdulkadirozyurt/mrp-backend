801037bb8131f997f00bfd656099c1fa069ba2fd
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2024-12-31 01:18:04 +0300

    Update User schema: Change phoneNumber field to be optional and retain uniqueness, allowing for more flexible user data management.

--- src/Entities/Concrete/User.ts
+++ src/Entities/Concrete/User.ts
@@ -10,7 +10,7 @@ const UserSchema = new Schema<IUser>(
     department: { type: String },
     position: { type: String },
     address: { type: String },
-    phoneNumber: { type: String, unique: true, sparse: true },
+    phoneNumber: { type: String, unique: true, required: false },
     employeeCode: { type: String, required: true, unique: true },
     role: {
       type: String,
